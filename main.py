e4c539edf21c05209e9c7cc62b9032df8cd4f75d
OmkarVuddanti <omkarvuddanti@gmail.com>
2019-09-27 23:02:49 +0530

    files created and main file completed

---
+++ main.py
@@ -0,0 +1,31 @@
+import add
+import sub
+import mult
+import div
+import sys
+
+global a, b
+
+
+args = sys.argv
+# print("enter operaton:\n1-add\n2-sub\n3-prod\n4-div\nchoice:\t")
+choice, a, b = args[1], int(args[2]), int(args[3])
+print(choice, a, b)
+# a = int(input("a: "))
+# b = int(input("b: "))
+ans = 0
+if choice=="1":
+	print("Adding", a, b)
+	ans = add.addition(a, b)
+if choice=="2":
+	print("Subtracting", a, b)
+	ans = sub.subtraction(a, b)
+if choice=="3":
+	print("Multiplying", a, b)
+	ans = mult.multiplication(a, b)
+if choice=="4":
+	print("Dividing", a, b)
+	ans = div.divide(a, b)
+
+print()
+print(ans)
